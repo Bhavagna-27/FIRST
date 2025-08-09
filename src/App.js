32c6a3347a8d3131b083f90e79801718d3e0fa50
2300033472@kluniversity.in <2300033472@kluniversity.in>
2025-08-09 20:42:03 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <p>This is a small change to update the GitHub URL.</p>
         <a
           className="App-link"
           href="https://reactjs.org"
